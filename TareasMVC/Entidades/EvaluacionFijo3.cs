20e1932322217201f62cb45558786e03d20b5cc6
Gonzalo anuar Pérez Aramburo <gonzalo.anuar13@outlook.com>
2025-10-28 13:06:54 -0600

    Generamos EvaluacionFijo3 que hereda de Evaluacion

--- TareasMVC/Entidades/EvaluacionFijo3.cs
+++ TareasMVC/Entidades/EvaluacionFijo3.cs
@@ -11,10 +11,23 @@ namespace TareasMVC.Entidades
     }
     public class EvaluacionFijo3: Evaluacion
     {
+        [Required]
         public EstadoRespuesta D1  { get; set; }
 
+        [MaxLength(500)]
+        public string TextoOtro1 { get; set; }
+
+        [Required]
+
+
         public EstadoRespuesta D2 { get; set; }
+        [MaxLength(500)]
+        public string TextoOtro2 { get; set; }
+
+        [Required]
 
         public EstadoRespuesta D3 { get; set; }
+        [MaxLength(500)]
+        public string TextoOtro3 { get; set; }
     }
 }
